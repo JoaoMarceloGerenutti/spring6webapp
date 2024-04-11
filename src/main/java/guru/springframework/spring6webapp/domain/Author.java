05ac820602f61d9c33e85a2733aaf16b6c3e6838
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-11 13:45:55 -0300

    Adding example of JPA Entities

---
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6webapp.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+}
